1cd1b9c8fff26e4af57a5f298204198ad991b1ac
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-04-02 15:52:45 +0900

    [FEAT] Toast 생성

--- app/src/main/java/org/sopt/android_hyorim_30th/MainActivity.kt
+++ app/src/main/java/org/sopt/android_hyorim_30th/MainActivity.kt
@@ -2,10 +2,19 @@ package org.sopt.android_hyorim_30th
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.sopt.android_hyorim_30th.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
