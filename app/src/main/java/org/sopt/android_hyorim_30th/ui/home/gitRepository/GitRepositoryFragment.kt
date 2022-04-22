5e9b4f83a5442e0383a56d0b3829fdcdb268f7f5
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-04-22 23:30:13 +0900

    [FEAT] set Data from GitRepositoryViewModel

--- app/src/main/java/org/sopt/android_hyorim_30th/ui/home/gitRepository/GitRepositoryFragment.kt
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/home/gitRepository/GitRepositoryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.viewModels
 import org.sopt.android_hyorim_30th.R
 import org.sopt.android_hyorim_30th.data.GitRepositoryData
 import org.sopt.android_hyorim_30th.databinding.FragmentGitRepositoryBinding
@@ -13,6 +14,7 @@ import org.sopt.android_hyorim_30th.ui.base.BaseFragment
 class GitRepositoryFragment :
     BaseFragment<FragmentGitRepositoryBinding>(R.layout.fragment_git_repository) {
     private lateinit var gitRepositoryAdapter: GitRepositoryAdapter
+    private val gitRepositoryViewModel: GitRepositoryViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,37 +23,21 @@ class GitRepositoryFragment :
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         initAdapter()
+        initViewModel()
         return binding.root
+    }
+
+    private fun initViewModel() {
+        binding.lifecycleOwner = this
     }
 
     private fun initAdapter() {
         binding.rvRepository.adapter =
             GitRepositoryAdapter(::onItemClick).also { gitRepositoryAdapter = it }
-        gitRepositoryAdapter.submitList(dummyData)
+        gitRepositoryAdapter.submitList(gitRepositoryViewModel.repository.value)
     }
 
     private fun onItemClick(repository: GitRepositoryData) {
         Toast.makeText(requireContext(), repository.title, Toast.LENGTH_SHORT).show()
-    }
-
-    companion object {
-        val dummyData = listOf(
-            GitRepositoryData(
-                "MVVMdiaryExample",
-                "MVVM 무작정 따라하기 스터디"
-            ),
-            GitRepositoryData(
-                "JsoupSample",
-                "Practice Crawling Meta Data (Web Scrapping)"
-            ),
-            GitRepositoryData(
-                "Architecture-Components",
-                "GuessTheWord-Starter"
-            ),
-            GitRepositoryData(
-                "RaspberryControl",
-                "2021 SSWU Smart System Capstone Design / Smart Multitap Project with Deep Learning."
-            )
-        )
     }
 }
