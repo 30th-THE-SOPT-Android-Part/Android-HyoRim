c1b8fc28c2458fa97b5493092a593b4075588684
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-04-22 17:55:39 +0900

    [ADD] Empty GitFollowerFragment & GitRepositoryFragment

---
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/home/gitFollower/GitFollowerFragment.kt
@@ -0,0 +1,8 @@
+package org.sopt.android_hyorim_30th.ui.home.gitFollower
+
+import org.sopt.android_hyorim_30th.R
+import org.sopt.android_hyorim_30th.databinding.FragmentGitFollowerBinding
+import org.sopt.android_hyorim_30th.ui.base.BaseFragment
+
+class GitFollowerFragment :
+    BaseFragment<FragmentGitFollowerBinding>(R.layout.fragment_git_follower)
---
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/home/gitRepository/GitRepositoryFragment.kt
@@ -0,0 +1,8 @@
+package org.sopt.android_hyorim_30th.ui.home.gitRepository
+
+import org.sopt.android_hyorim_30th.R
+import org.sopt.android_hyorim_30th.databinding.FragmentGitRepositoryBinding
+import org.sopt.android_hyorim_30th.ui.base.BaseFragment
+
+class GitRepositoryFragment :
+    BaseFragment<FragmentGitRepositoryBinding>(R.layout.fragment_git_repository)
