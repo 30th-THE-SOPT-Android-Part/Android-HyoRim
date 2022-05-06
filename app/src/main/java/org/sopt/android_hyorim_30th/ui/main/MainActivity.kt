4b5ce955ffbca64c5f30a2469bb49cdeee444fed
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-06 18:31:29 +0900

    [feat] viewPager와 fragment 연결

--- app/src/main/java/org/sopt/android_hyorim_30th/ui/main/MainActivity.kt
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/main/MainActivity.kt
@@ -1,40 +1,32 @@
 package org.sopt.android_hyorim_30th.ui.main
 
+import android.os.Bundle
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.android_hyorim_30th.R
 import org.sopt.android_hyorim_30th.databinding.ActivityMainBinding
 import org.sopt.android_hyorim_30th.ui.base.BaseActivity
+import org.sopt.android_hyorim_30th.ui.main.camera.CameraFragment
+import org.sopt.android_hyorim_30th.ui.main.home.HomeFragment
+import org.sopt.android_hyorim_30th.ui.main.profile.ProfileFragment
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainViewModel: MainViewModel by viewModels()
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding.viewModel = mainViewModel
-//        binding.lifecycleOwner = this
-//        setDefaultFragment()
-//        setSelectedFragment()
-//    }
-//
-//    private fun setDefaultFragment() {
-//        setFragmentWith<GitFollowerFragment>()
-//    }
-//
-//    private fun setSelectedFragment() {
-//        mainViewModel.selectedFragment.observe(this) {
-//            when (it) {
-//                GitViewType.GIT_FOLLOWER -> setFragmentWith<GitFollowerFragment>()
-//                GitViewType.GIT_REPOSITORY -> setFragmentWith<GitRepositoryFragment>()
-//                else -> throw IllegalArgumentException()
-//            }
-//        }
-//    }
-//
-//    private inline fun <reified T : Fragment> setFragmentWith() {
-//        supportFragmentManager.commit {
-//            replace<T>(R.id.container)
-//        }
-//    }
+    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initAdapter()
+        setViewPagerFragment()
+    }
+
+    private fun initAdapter() {
+        binding.vpMain.adapter = MainViewPagerAdapter(this).also { mainViewPagerAdapter = it }
+    }
+
+    private fun setViewPagerFragment() {
+        mainViewPagerAdapter.fragmentList =
+            listOf(ProfileFragment(), HomeFragment(), CameraFragment())
+    }
 }
