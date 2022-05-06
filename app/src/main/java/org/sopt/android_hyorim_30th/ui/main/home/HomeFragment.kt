e338ac7ed16e852fa348dff26383dad2f5dc50c6
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-06 23:49:43 +0900

    [ui] home, camera fragment 작업

--- app/src/main/java/org/sopt/android_hyorim_30th/ui/main/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/main/home/HomeFragment.kt
@@ -2,12 +2,34 @@ package org.sopt.android_hyorim_30th.ui.main.home
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.tabs.TabLayoutMediator
 import org.sopt.android_hyorim_30th.R
 import org.sopt.android_hyorim_30th.databinding.FragmentHomeBinding
 import org.sopt.android_hyorim_30th.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private lateinit var gitTabViewPagerAdapter: GitTabViewPagerAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.lifecycleOwner = this
+        initAdapter()
+        setViewPagerFragment()
+        initTabLayout()
+    }
+
+    private fun initAdapter() {
+        binding.vpFollow.adapter =
+            GitTabViewPagerAdapter(this).also { gitTabViewPagerAdapter = it }
+    }
+
+    private fun setViewPagerFragment() {
+        gitTabViewPagerAdapter.fragmentList = listOf(TempFragment1(), TempFragment2())
+    }
+
+    private fun initTabLayout() {
+        val tabLabel = listOf("팔로잉", "팔로워")
+
+        TabLayoutMediator(binding.tlFollow, binding.vpFollow) { tab, position ->
+            tab.text = tabLabel[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/main/home/TempFragment2.kt
@@ -0,0 +1,19 @@
+package org.sopt.android_hyorim_30th.ui.main.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.sopt.android_hyorim_30th.R
+
+class TempFragment2 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_temp2, container, false)
+    }
+}
