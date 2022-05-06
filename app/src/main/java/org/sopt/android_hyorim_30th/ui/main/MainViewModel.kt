3a17fb962bfbc08a9f7cee3c3d600561097446a9
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-06 21:51:31 +0900

    [chore] dummy data 변경

--- app/src/main/java/org/sopt/android_hyorim_30th/ui/main/MainViewModel.kt
+++ app/src/main/java/org/sopt/android_hyorim_30th/ui/main/MainViewModel.kt
@@ -7,10 +7,10 @@ import org.sopt.android_hyorim_30th.data.type.GitViewType
 
 class MainViewModel : ViewModel() {
     val profileImage = R.drawable.loopy
-    val userName = "루피"
+    val userName = "김루피"
     val userAge = 4
     val userMBTI = "BABY"
-    val userIntroduction = "군침이 싹도누 🙃"
+    val userIntroduction = "나는야 잔망루피! 군침이 싹도누 🙃"
     var selectedFragment = MutableLiveData(GitViewType.GIT_FOLLOWER)
 
     fun onClickFollowerBtn() {
